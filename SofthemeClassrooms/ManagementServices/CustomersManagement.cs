c7e27c717a063d28a3714fe57c3e3ea368adf1d8
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-20 18:29:00 +0200

    Rearranged folder structure

---
+++ SofthemeClassrooms/ManagementServices/CustomersManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+
+
+namespace ManagementServices
+{
+    public static class CustomersManagement
+    {
+        public static List<Customers> getAllCustomers()
+        {
+            var context = new MyDBContext();
+            List<Customers> customers = context.Customers.ToList();
+            return customers;
+        }
+    }
+}
