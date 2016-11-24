cd4f12ff879282bf327cc1691e6a5003e2fbecb1
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-24 05:22:05 +0200

    Profile, Users, Event controllers + viewModels

---
+++ SofthemeClassrooms/MVC/Controllers/EventController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class EventController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(int id)
+        {
+            return new EmptyResult();
+        }
+
+        // Returns a partial view - overlay with event info
+        [HttpGet]
+        public ActionResult EventInfoOverlay(int Id)
+        {
+            return new EmptyResult();
+        }
+
+        // Returns a partial view - overlay with event edit option
+        [HttpGet]
+        public ActionResult EventEditOverlay(int Id)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Cancel(int id)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Add(EventEditViewModel eventInfo)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Modify(EventEditViewModel eventInfo)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Subscribe(string eMail)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult CancelSubscription(string eMail)
+        {
+            return new EmptyResult();
+        }
+    }
+}
