4a427b4bb19db30d2b0bda3306d816674c308005
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-24 04:45:49 +0200

    Added accees attributes for schedule actions

--- SofthemeClassrooms/MVC/Controllers/ScheduleController.cs
+++ SofthemeClassrooms/MVC/Controllers/ScheduleController.cs
@@ -9,18 +9,21 @@ namespace WebApplication1.Controllers
     public class ScheduleController : Controller
     {
         // Render the page
+        [HttpGet]
         public ActionResult Index()
         {
             return new EmptyResult();
         }
 
         // Get data about events for a specific date
+        [HttpGet]
         public ActionResult GetSchedulerState(DateTime date)
         {
             return new EmptyResult();
         }
 
         // Get equipment info for a specific room
+        [HttpGet]
         public ActionResult GetRoomInfo(int roomId)
         {
             return new EmptyResult();
