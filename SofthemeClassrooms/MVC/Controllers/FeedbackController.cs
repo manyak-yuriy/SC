e20e6e9e8c0b335baa3bcecf847ae1afb1603024
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-24 04:52:34 +0200

    Feedback controller and ViewModels

---
+++ SofthemeClassrooms/MVC/Controllers/FeedbackController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(FeedbackViewModel feedback)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return new EmptyResult();
+        }
+    }
+}
