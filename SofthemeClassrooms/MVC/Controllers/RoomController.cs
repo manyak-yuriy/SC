11d54f9d6b51801330c903b7866f7f6982068cfc
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-24 04:41:30 +0200

    Room controller

---
+++ SofthemeClassrooms/MVC/Controllers/RoomController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class RoomController : Controller
+    {
+        // Save info about capacity, number of equipment items
+        [HttpPost]
+        public ActionResult SaveProperties(RoomPropViewModel roomProp)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Close(int roomId)
+        {
+            return new EmptyResult();
+        }
+        [HttpPost]
+        public ActionResult Open(int roomId)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int roomId)
+        {
+            return new EmptyResult();
+        }
+
+        // Returns information about events
+        [HttpGet]
+        public ActionResult GetSchedulerState(int roomId, DateTime fromDate, DateTime toDate)
+        {
+            return new EmptyResult();
+        }
+
+        // Returns room capacity, number of items
+        [HttpGet]
+        public ActionResult GetProperties(int roomId)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpGet]
+        public ActionResult Index(int roomId)
+        {
+            return new EmptyResult();
+        }
+    }
+}
