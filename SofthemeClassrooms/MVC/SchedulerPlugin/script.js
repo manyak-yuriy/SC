02573c1df68fdf87538a71689bc00e1baf32fcbc
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-27 21:59:51 +0200

    Integrated scheduler plugin

---
+++ SofthemeClassrooms/MVC/SchedulerPlugin/script.js
@@ -0,0 +1,19 @@
+
+jQuery("#schedule").timeSchedule({
+  rows : {
+    '1' : {
+      title : 'Title Area',
+      schedule:[{
+        start:'09:00',
+        end:'12:00',
+        text:'Text Area',
+        data:{}
+      },{
+        start:'11:00',
+        end:'14:00',
+        text:'Text Area',
+        data:{}
+      }]
+    },},
+});
+
