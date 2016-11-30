0e6d6c050c0aed653977814e6f2e2e31e5be4b61
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-30 20:15:40 +0200

    Finished auto-save integration

--- SofthemeClassrooms/MVC/Scripts/RoomsScheme/RoomsScheme.js
+++ SofthemeClassrooms/MVC/Scripts/RoomsScheme/RoomsScheme.js
@@ -1,10 +1,117 @@
-﻿var resizeOptions = {
-    autoHide: true, ghost: true
-}
+﻿var posData = {};
+
+var resizeOptions = {
+                        autoHide: true, ghost: true
+                    }
+
+var loadPos = 
+                function() 
+                {
+
+                  $.get("https://api.myjson.com/bins/19hhe", function(data, textStatus, jqXHR) {
+                        //alert(textStatus);
+                        posData = data;
+                        //alert(JSON.stringify(posData));
+
+                        $(".draggable").each( 
+                            function() 
+                            { 
+                                //console.log($(this).text()); 
+                                var key = $(this).attr("id");
+                                
+
+                                $(this).css("left", posData[key]["left"]);
+                                $(this).css("top", posData[key]["top"]);
+
+                                $img = $(this).find("img");
+                                $img.resizable( "destroy" ).width(posData[key]["width"]).resizable(resizeOptions); 
+                                $img.resizable( "destroy" ).height(posData[key]["height"]).resizable(resizeOptions);  
+                            }
+                        );
+
+
+                  });
+                  
+                  console.log();
+                }
+
+
+var savePos = 
+                function() 
+                {
+                  
+                  $(".draggable").each( 
+                    function() 
+                    { 
+                      //console.log($(this).text()); 
+                      var key = $(this).attr("id");
+                      posData[key] = {
+                                         left: $(this).css("left"), 
+                                         top: $(this).css("top"), 
+                                         width: $(this).find("img").width(), 
+                                         height: $(this).find("img").height()
+                                     };
+                    }
+                  );
+
+                   
+                  var jsonData = JSON.stringify(posData);
+/*
+                  $.ajax({
+                        url:"https://api.myjson.com/bins",
+                        type:"POST",
+                        data: jsonData,
+                        contentType:"application/json; charset=utf-8",
+                        dataType:"json",
+                        success: function(data, textStatus, jqXHR){
+                            var str = data["uri"];
+                            var ind = str.lastIndexOf("/");
+                            var id = str.substring(ind + 1); 
+                            alert(id);
+                        }
+                  });
+*/
+
+                  $.ajax({
+                        url:"https://api.myjson.com/bins/19hhe",
+                        type:"PUT",
+                        data: jsonData,
+                        contentType:"application/json; charset=utf-8",
+                        dataType:"json",
+                        success: function(data, textStatus, jqXHR){
+                            
+                            //alert(jsonData);
+                        }
+                  });
+                
+                  console.log();
+                }
+
 
 $(  function() 
-{
-    $(".draggable").draggable();
+    {
+            //loadPos();
 
-    $(".resizeable").resizable(resizeOptions);
-})
+            $(".draggable" ).draggable();
+
+            $(".resizeable" ).resizable(resizeOptions);
+
+            //$("#map" ).resizable(resizeOptions);
+
+            $('#posSaver').click(
+                () => { savePos(); }               
+            );
+
+            $('#posLoader').click(
+                () => { loadPos(); }
+            );
+
+            $('#captionHide').click(
+                () => {
+                          $vis = 'hidden';
+                          $(".caption").css('visibility', $vis); 
+                      }
+            );
+
+   } 
+);
