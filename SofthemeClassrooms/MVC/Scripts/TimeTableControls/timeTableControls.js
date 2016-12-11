c2a859868eb5aacc9b73d8f4089fd61a108a0b3a
manyak-yuriy <manyak.yuriy@gmail.com>
2016-12-11 12:35:16 +0200

    Hide and show timetable quick nav controls depending on the visibility of sliders

---
+++ SofthemeClassrooms/MVC/Scripts/TimeTableControls/timeTableControls.js
@@ -0,0 +1,18 @@
+﻿function checkTimeTableQuickNavState() {
+    var scrollOffset = $(".scrollMe").smoothDivScroll("getScrollerOffset");
+    var totalWidth = $('.scrollWrapper').width();
+    var currTimeSliderleft = parseInt($('#time-now-wrapper').css('left'));
+
+
+    $('#rightTodayInTimeTable').css('visibility', 'hidden');
+    $('#leftTodayInTimeTable').css('visibility', 'hidden');
+
+    // slider is hidden to the left side
+    if (currTimeSliderleft < 0) {
+        $('#leftTodayInTimeTable').css('visibility', 'visible');
+    }
+        // slider is hidden to the right side
+    else if (currTimeSliderleft > totalWidth) {
+        $('#rightTodayInTimeTable').css('visibility', 'visible');
+    }
+}
