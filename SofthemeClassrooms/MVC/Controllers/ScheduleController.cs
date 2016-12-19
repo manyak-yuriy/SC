9753929c3e081685e553c1c574d761461870026d
manyak-yuriy <manyak.yuriy@gmail.com>
2016-12-19 13:55:04 +0200

    Added check for business hours

--- SofthemeClassrooms/MVC/Controllers/ScheduleController.cs
+++ SofthemeClassrooms/MVC/Controllers/ScheduleController.cs
@@ -210,6 +210,8 @@ namespace WebApplication1.Controllers
             if (eventModel.End - eventModel.Start < new TimeSpan(0, 20, 0))
                 errors.Errors.Add("Событие не может быть короче 20 минут");
 
+            if (eventModel.End.Hour > 21 || (eventModel.End.Hour == 21 && eventModel.End.Minute != 0) || eventModel.Start.Hour < 9)
+                errors.Errors.Add("Событие не может быть за пределами рабочих часов");
 
             if (classRoom == null)
             {
@@ -276,6 +278,8 @@ namespace WebApplication1.Controllers
             if (eventModel.End - eventModel.Start < new TimeSpan(0, 20, 0))
                 errors.Errors.Add("Событие не может быть короче 20 минут");
 
+            if (eventModel.End.Hour > 21 || (eventModel.End.Hour == 21 && eventModel.End.Minute != 0) || eventModel.Start.Hour < 9)
+                errors.Errors.Add("Событие не может быть за пределами рабочих часов");
 
             if (classRoom == null)
             {
