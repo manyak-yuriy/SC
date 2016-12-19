5a675419dc4e324ea76112c3921526f194d162b8
manyak-yuriy <manyak.yuriy@gmail.com>
2016-12-19 11:42:20 +0200

    Styled display event partial

--- SofthemeClassrooms/MVC/Controllers/EventController.cs
+++ SofthemeClassrooms/MVC/Controllers/EventController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using DataAccessLayer;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models.Schedule;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +14,48 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public ActionResult Index(int eventId)
         {
-            return View();
+            return View(eventId);
+        }
+
+        [HttpGet]
+        public ActionResult GetDisplayEventPartial(int Id)
+        {
+            var db = new ApplicationDbContext();
+
+            var eventEntity = db.Event.Where(e => e.Id == Id).FirstOrDefault();
+
+            if (eventEntity == null)
+                throw new NullReferenceException("There's no event with specified id!");
+
+
+            bool isAuthorized = false;
+            bool isAdmin = User.IsInRole("admin");
+            string userId = User.Identity.GetUserId();
+
+            if (isAdmin || eventEntity.ApplicationUserID == userId)
+                isAuthorized = true;
+
+            if (!eventEntity.IsPublic && !isAuthorized)
+                throw new AccessViolationException("Not enough rights to view the private event");
+
+            DisplayEventPartialViewModel model = new DisplayEventPartialViewModel();
+
+            model.Id = eventEntity.Id;
+            model.CanEdit = isAuthorized;
+            model.AllowSubscription = (bool)eventEntity.AllowSubscription;
+            model.DateStart = eventEntity.DateStart;
+            model.DateEnd = eventEntity.DateEnd;
+            model.Title = eventEntity.Title;
+            model.Description = eventEntity.Description;
+
+            if (eventEntity.OrganizerName != null)
+                model.OrganizerName = eventEntity.OrganizerName;
+            else
+                model.OrganizerName = (eventEntity.Organizer == null) ? "" : eventEntity.Organizer.UserName;
+
+            model.VisitorCount = eventEntity.ForeignVisitor.Count;
+
+            return PartialView("_DisplayEventPartial", model);
         }
 
     }
