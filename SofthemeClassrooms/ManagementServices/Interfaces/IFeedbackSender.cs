519fc28c84db2c28a9ce4dfa12d51938dba133d2
manyak-yuriy <manyak.yuriy@gmail.com>
2017-03-01 10:27:23 +0200

    Revert "breaking bad"
    This reverts commit c2b4c0c7de833e5f889df0de91549924e6570812.

---
+++ SofthemeClassrooms/ManagementServices/Interfaces/IFeedbackSender.cs
@@ -0,0 +1,14 @@
+﻿using ManagementServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementServices.Interfaces
+{
+    public interface IFeedbackSender
+    {
+        void SendFeedback(FeedBackDTO feedback);
+    }
+}
